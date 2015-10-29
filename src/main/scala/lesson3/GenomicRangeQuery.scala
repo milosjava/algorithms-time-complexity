3991bfa44b0eafc5480a097cea8829ef99979b5e
Milos Grubjesic <milosjava@gmail.com>
2015-10-29 18:11:40 +0100

    add GenomicRangeQuery to lesson3

---
+++ src/main/scala/lesson3/GenomicRangeQuery.scala
@@ -0,0 +1,33 @@
+package lesson3
+
+//test text: https://codility.com/programmers/task/genomic_range_query
+//performance : https://codility.com/demo/results/trainingEUR4XP-425/
+
+object GenomicRangeQuery {
+
+  def solution(S: String, P: Array[Int], Q: Array[Int]): Array[Int] = {
+
+
+    val resp = for(ind <- 0 to P.length-1) yield {
+
+      val sub= S.substring(P(ind),Q(ind)+1)
+
+
+      var factor = 4
+
+      if(sub.contains("A")) {factor=1}
+      else{
+        if(sub.contains("C")) {factor=2}
+        else{
+          if(sub.contains("G")) {factor=3}
+        }
+      }
+      factor
+
+    }
+
+    return resp.toArray
+
+  }
+
+}
---
+++ src/test/scala/lesson3/GenomicRangeQuerySpec.scala
@@ -0,0 +1,31 @@
+package lesson3
+
+import org.scalatest.FunSuite
+
+/**
+ * Created by sensei on 10/29/15.
+ */
+class GenomicRangeQuerySpec extends FunSuite{
+
+
+
+  test("sample data"){
+
+    val P = Array(2,5,0)
+    val Q = Array(4,5,6)
+
+    val S = "CAGCCTA"
+
+
+
+    val res = GenomicRangeQuery.solution(S,P,Q)
+
+
+    assert(res.length == 3 )
+
+    assert( res.sorted.deep == Array(1,2,4).deep)
+
+
+  }
+
+}
