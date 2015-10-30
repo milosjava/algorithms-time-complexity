c89d9b678bac53bc4ce2033dd4ecc1b914d5e97a
Milos Grubjesic <milosjava@gmail.com>
2015-10-30 16:47:40 +0100

    add lesson6 Dominator

---
+++ src/main/scala/lesson6/Dominator.scala
@@ -0,0 +1,42 @@
+package lesson6
+
+//todo improve 91%
+//task text:  https://codility.com/programmers/task/dominator
+//performance: https://codility.com/demo/results/trainingRY8UX4-NY2/
+object Dominator {
+
+
+  def solution(A: Array[Int]): Int = {
+
+    val N = A.length
+
+    //empty array , no dominator
+    if(N == 0) return -1
+
+    //just one element so this element is dominator
+    if(N == 1) return A(0)
+
+
+    //group values
+    val count = A.groupBy(identity).mapValues(_.size)
+
+    //find which one shows often
+    val maxOccurence = count.valuesIterator.max
+
+
+    if(N/2 < maxOccurence){
+
+      //get key from the map which is our dominator
+      val value = count.find(_._2==maxOccurence).get._1
+
+      //get the first index for dominator
+      return A.indexOf(value)
+
+    }else{
+      //no dominator
+      return -1
+    }
+
+  }
+
+}
---
+++ src/test/scala/lesson6/DominatorSpec.scala
@@ -0,0 +1,55 @@
+package lesson6
+
+import org.scalatest.FunSuite
+
+/**
+ * Created by sensei on 10/30/15.
+ */
+class DominatorSpec extends FunSuite {
+
+
+  test("sample data"){
+
+    val A = Array(3,4,3,2,3,-1,3,3)
+
+    val res  = Dominator.solution(A)
+
+    assert(Array(0, 2, 4, 6 ,7).contains(res))
+
+  }
+
+  test("empty"){
+
+    val A = Array[Int]()
+
+    val res  = Dominator.solution(A)
+
+    assert(res == -1)
+
+  }
+
+  test("one element"){
+
+    val A = Array(66)
+
+    val res  = Dominator.solution(A)
+
+    assert(res == 66)
+
+  }
+
+  test("2 elements"){
+
+    val A = Array(33,44)
+
+    val res  = Dominator.solution(A)
+
+    assert(res == -1)
+
+
+  }
+
+
+
+
+}
