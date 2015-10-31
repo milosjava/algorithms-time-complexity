03f3d0a00e774241da17858c34a147ab50bbf07b
Milos Grubjesic <milosjava@gmail.com>
2015-10-31 18:59:44 +0100

    add lesson13  min abs sum

---
+++ src/main/scala/lesson13/MinAbsSumOfTwo.scala
@@ -0,0 +1,27 @@
+package lesson13
+
+
+//todo improve 45%
+//task text: https://codility.com/programmers/task/min_abs_sum_of_two
+//performance: https://codility.com/demo/results/trainingUBTVX4-KYS/
+
+object MinAbsSumOfTwo {
+
+
+  def solution(A: Array[Int]): Int = {
+
+
+    val itselfAbsSum =A.map(p=> math.abs(p+p))
+
+
+    val othersAbsSum  = (A.combinations(2).toArray).map(p=> math.abs(p(0)+p(1)))
+
+
+    val result = (itselfAbsSum ++ othersAbsSum).min
+
+    result
+
+  }
+
+
+}
---
+++ src/test/scala/lesson13/MinAbsSumOfTwoSpec.scala
@@ -0,0 +1,20 @@
+package lesson13
+
+import org.scalatest.FunSuite
+
+/**
+ * Created by sensei on 10/31/15.
+ */
+class MinAbsSumOfTwoSpec extends  FunSuite{
+
+  test("sample code"){
+
+    val A = Array(-8,4,5,-10,3)
+
+    assert(MinAbsSumOfTwo.solution(A) == 3)
+
+
+  }
+
+
+}
