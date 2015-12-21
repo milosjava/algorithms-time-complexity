4d3269b55374d04d8a574626b0c0b22810a25852
milos <milosjava@gmail.com>
2015-12-21 17:46:47 +0100

    add git ignore

--- src/main/scala/CaterpillarMethod/MinAbsSumOfTwo.scala
+++ src/main/scala/CaterpillarMethod/MinAbsSumOfTwo.scala
@@ -2,6 +2,7 @@ package CaterpillarMethod
 
 
 //todo improve 45%
+
 //task text: https://codility.com/programmers/task/min_abs_sum_of_two
 //performance: https://codility.com/demo/results/trainingUBTVX4-KYS/
 
@@ -10,12 +11,9 @@ object MinAbsSumOfTwo {
 
   def solution(A: Array[Int]): Int = {
 
-
     val itselfAbsSum =A.map(p=> math.abs(p+p))
 
-
     val othersAbsSum  = (A.combinations(2).toArray).map(p=> math.abs(p(0)+p(1)))
-
 
     val result = (itselfAbsSum ++ othersAbsSum).min
 
